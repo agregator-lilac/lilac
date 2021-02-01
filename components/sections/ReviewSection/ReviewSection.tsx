65e796148f6f314336f09ed981c755577e0fc660
Fedotov Aleksey <far@rio-soft.ru>
2021-02-01 15:56:25 +0500

    переделал запрос отзывов на фронте

--- components/sections/ReviewSection/ReviewSection.tsx
+++ components/sections/ReviewSection/ReviewSection.tsx
@@ -5,13 +5,24 @@ import {Slider} from './Slider';
 
 import styles from './styles.module.scss';
 import {IReview} from '@/types/reviews.types';
+import useSWR from 'swr';
 
 interface IProps {
 	data?: IReview[];
 }
 
-const ReviewSection: FC<IProps> = ({data}) => {
-	const [reviews, setReviews] = useState<IReview[]>(data);
+const ReviewSection: FC<IProps> = () => {
+	const [reviews, setReviews] = useState<IReview[]>([]);
+
+	const {data} = useSWR<IReview[]>('/api/reviews', url => fetch(url).then(data => data.json()));
+
+	if (!data) {
+		return <div>loading</div>;
+	}
+	
+	if (data && reviews.length === 0) {
+		setReviews(data);
+	}
 
 	return (
 		<section className={`container ${styles.root}`}>
